f39931cd48ffffc86061b9f0b02a27beac4cc49e
Andrey Radionov <andyomsk@gmail.com>
2017-01-06 19:15:06 +0600

    Added 3 empty servlets

---
+++ src/main/java/com/aradionov/socketchat/servlets/RegisterServlet.java
@@ -0,0 +1,17 @@
+package com.aradionov.socketchat.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Andrey Radionov
+ */
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
