35330b8180d0580887be80f2d7475b11b4dd4d9c
Andrey Radionov <andyomsk@gmail.com>
2017-01-06 20:41:48 +0600

    DBManager

---
+++ src/main/java/com/aradionov/socketchat/dao/DBManager.java
@@ -0,0 +1,50 @@
+package com.aradionov.socketchat.dao;
+
+
+import com.aradionov.socketchat.model.Message;
+import com.aradionov.socketchat.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * @author Andrey Radionov
+ */
+public class DBManager {
+    private static final String hibernate_show_sql = "true";
+    private static final String hibernate_hbm2ddl_auto = "update";
+
+    private final SessionFactory sessionFactory;
+
+    public DBManager() {
+        Configuration configuration = getH2Configuration();
+        sessionFactory = createSessionFactory(configuration);
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    private Configuration getH2Configuration() {
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(Message.class);
+
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:./h2_chat_db");
+        configuration.setProperty("hibernate.connection.username", "superuser");
+        configuration.setProperty("hibernate.connection.password", "reallyhardpass11");
+        configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
+        configuration.setProperty("hibernate.hbm2ddl.auto", hibernate_hbm2ddl_auto);
+        return configuration;
+    }
+
+    private SessionFactory createSessionFactory(Configuration configuration) {
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+        builder.applySettings(configuration.getProperties());
+        ServiceRegistry serviceRegistry = builder.build();
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+}
